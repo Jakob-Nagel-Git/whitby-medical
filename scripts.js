73bf0ad26b167ff2b49d19cc3dcb41faced03754
Jakob Nagel <Jakob1211@hotmail.com>
2022-05-31 14:30:06 -0400

    Init

---
+++ scripts.js
@@ -0,0 +1,54 @@
+function mapsSelector() {
+    if /* if we're on iOS, open in Apple Maps */
+      ((navigator.platform.indexOf("iPhone") != -1) || 
+       (navigator.platform.indexOf("iPad") != -1) || 
+       (navigator.platform.indexOf("iPod") != -1))
+      window.open("http://maps.apple.com/?q=Whitby+Medical+Walk+In+Clinic");
+  else /* else use Google */
+      window.open("https://goo.gl/maps/ykWDgMR4Qr869VLJ6");
+}
+
+
+$(document).ready(function(){
+    $('.burgermenu').on('click', function(){
+        $('.mob-nav').toggle();
+    })
+});
+
+
+// Select all links with hashes
+$('a[href*="#"]')
+  // Remove links that don't actually link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+      && 
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000, function() {
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          };
+        });
+      }
+    }
+  });
